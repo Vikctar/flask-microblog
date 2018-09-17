84cf6a3ae2920af2b9f4ca1b71338b1717e05eb3
Vikctar <vikc.steve@gmail.com>
2018-09-17 18:51:41 +0300

    Use and apply the config file that contains our 'hard to guess key :p'

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from config import Config
 """
 This script creates the application object as an instance of class Flask
 imported from the flask package. 
 """
 
 app = Flask(__name__)
+
+# Read and apply the config file.
+app.config.from_object(Config)
 
 from app import routes
