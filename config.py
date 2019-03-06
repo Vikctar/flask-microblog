e7d6bfa2247c307be483e4d82ca33cc0105bc243
Vikctar <vikc.steve@gmail.com>
2019-03-06 15:52:35 +0300

    form templates and views

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     """
-    The SECRET_KEY config is a very important item. It's used as cryto
+    The SECRET_KEY config is a very important item. It's used as crypto
     key, in the defence against CSRF (Cross-Site Request Forgery).
     The app will look in the os environment for a corresponding value. If not found
     a hardcoded value is used instead. This is for dev environment purposes only.
